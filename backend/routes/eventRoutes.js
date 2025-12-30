9ba16c232df2de9a30decdd1074d6899e9010a04
farhanshahriars <112596025+farhanshahriars@users.noreply.github.com>
2025-12-30 18:33:17 +0600

    Update eventRoutes.js

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -4,11 +4,11 @@ const eventController = require('../controllers/eventController');
 const auth = require('../middleware/auth');
 const { validateEvent } = require('../middleware/validation');
 
-// Public routes
+
 router.get('/', eventController.getAllEvents);
 router.get('/:id', eventController.getEvent);
 
-// Protected routes (require authentication)
+
 router.post('/', auth, validateEvent, eventController.createEvent);
 router.put('/:id', auth, eventController.updateEvent);
 router.delete('/:id', auth, eventController.deleteEvent);
